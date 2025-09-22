f1be90ec24691729c506434ca17d121156a7a5fa
masaki921 <masakiogura.23@gmail.com>
2025-09-22 15:27:52 +0900

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,13 @@
+
+export interface Log {
+  date: string; // YYYY-MM-DD
+  timeSpent: number; // in minutes
+}
+
+export interface Task {
+  id: string;
+  name: string;
+  targetTime: number; // in minutes
+  logs: Log[];
+  createdAt: string;
+}
